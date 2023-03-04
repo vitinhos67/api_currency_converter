1d22fd78a15724090a2d57ef9a99ff26e8023073
Victor Santos <santtosvictor42@gmail.com>
2023-03-04 16:50:40 -0300

    chore: initial configs and start server with fastify

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import fastify from "fastify";
+import { FastifyInstance } from "fastify/types/instance";
+import pino from "pino";
+
+const app: FastifyInstance = fastify({
+      logger: pino({ level: 'info' })
+})
+
+
+app.listen({
+    port: 3000
+}, () => {
+    app.log.info('listening port 3000')
+})
