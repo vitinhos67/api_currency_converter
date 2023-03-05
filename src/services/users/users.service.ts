db17049b9e9afda7916e8fd8148deb9a47bc7e9d
Victor Santos <santtosvictor42@gmail.com>
2023-03-05 15:21:53 -0300

    chore: verification if email already in use

--- src/services/users/users.service.ts
+++ src/services/users/users.service.ts
@@ -2,6 +2,7 @@ import catchErrorsFunctions from '../../common/utils/catchErrorsFunction';
 import UserModel from '../../model/User.model';
 import { UserModelInterface, UserServiceInterface } from '../../interfaces/users/userModel.interface';
 import { User } from '../../interfaces/users/User.interface';
+import { InvalidArgumentError } from '../err/Errors';
 
 class UserService implements UserServiceInterface {
     private readonly userModel: UserModelInterface;
@@ -27,10 +28,29 @@ class UserService implements UserServiceInterface {
 
     async store(user: User) {
         try {
+            const errors = [];
+
+            const emailAlreadyInUse = await this.findEmail(user.email);
+
+            if (emailAlreadyInUse) {
+                errors.push('Error: Email Already in use');
+            }
+
+            if (errors.length) {
+                throw new InvalidArgumentError(JSON.stringify(errors));
+            }
+
             const saveUserDb = await this.userModel.store(user);
-            console.log(saveUserDb);
 
             return saveUserDb;
+        } catch (error) {
+            catchErrorsFunctions(error);
+        }
+    }
+
+    private async findEmail(email: string) {
+        try {
+            return await this.userModel.findByEmail(email);
         } catch (error) {
             catchErrorsFunctions(error);
         }
